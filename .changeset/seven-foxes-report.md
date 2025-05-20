161ba8917c2a55725f323f2adc3efb1b980db17a
Matthew Volk <volkmattj@gmail.com>
2025-05-20 16:04:45 -0500

    add new feature

---
+++ .changeset/seven-foxes-report.md
@@ -0,0 +1,5 @@
+---
+"web": patch
+---
+
+Add new feature to `main`
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -32,6 +32,7 @@ export default function Home() {
           priority
         />
         <ol>
+          <li>New feature!</li>
           <li>
             Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
